d0534c2d9743b99344cd69fe6eca9c034cfde01d
constantinoi <pescaalma@gmail.com>
2021-01-10 18:49:14 -0400

    add Map

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,13 +2,14 @@ import "./styles.css";
 import StepsHeader from "./StepsHeader";
 import ProductsList from "./ProductsList";
 import { useEffect, useState } from "react";
-import { Product } from "./types";
+import { OrderLocationdata, Product } from "./types";
 import { fetchProduct } from "../api";
+import OrderLocation from "./OrderLocation";
 
 function Orders(){
 
     const [products, setProducts] = useState<Product[]>([]);
-    console.log(products)
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     useEffect(() => {
         fetchProduct().then(response => setProducts(response.data)).catch(error => console.log(error))
@@ -18,8 +19,9 @@ function Orders(){
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>     
-    )
+    )   
 }
 
 export default Orders;
