d68246fa3796207140ea819d3187ea9736718708
constantinoi <pescaalma@gmail.com>
2021-01-10 00:48:39 -0400

    finally Cards

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,7 +1,7 @@
 function StepsHeader(){
     return (
         <header className="orders-steps-container">
-               <div className="orders-steos-content">
+               <div className="orders-steps-content">
                     <h1 className="steps-title" > SIGA AS<br/>ETAPAS</h1>
                     <ul className="steps-items">
                         <li>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
 import "./styles.css";
 import StepsHeader from "./StepsHeader";
+import ProductsList from "./ProductsList";
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import { fetchProduct } from "../api";
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products)
+
+    useEffect(() => {
+        fetchProduct().then(response => setProducts(response.data)).catch(error => console.log(error))
+    },[])
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>     
     )
 }
