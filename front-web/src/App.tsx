d83101d3a775f11c65af86ea4ac94cf32abc7c86
constantinoi <pescaalma@gmail.com>
2021-01-09 22:43:25 -0400

    add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Hello from './Hello';
+import Navbar from './Navbar/index';
 
 function App() {
   return (
     <div >
-      <Hello/>
+      <Navbar />
     </div>
   );
 }
